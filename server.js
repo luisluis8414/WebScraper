87f013bb8fb3ac21ca72d9924de1f36a0dcf3fb0
luisluis8414 <luis.wehrberger2@gmail.com>
2023-02-24 10:35:51 +0100

    Fixed bug with the transfer of the Excel File

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express'); // Importieren des Express-Frameworks
-const multer  = require('multer'); // Importieren des Multer-Moduls
-const path = require('path'); // Importieren des Path-Moduls
+const express = require('express'); 
+const multer  = require('multer'); 
+const path = require('path');
 const script = require('./script');
 
-const app = express(); // Erstellen einer neuen Express-App
+const app = express(); 
 
 async function execute(filePath)
 {
     const emails= await script.findAllEmails(filePath);
     console.log(emails);
 }
-const upload = multer({ // Konfigurieren von Multer
-  storage: multer.diskStorage({ // Verwenden von Multer.diskStorage als Speicheroption
-    destination: './upload/', // Speicherort für die hochgeladene Datei
-    filename: function (req, file, cb) { // Konfigurieren des Dateinamens
-      cb(null, file.originalname); // Verwenden des ursprünglichen Dateinamens der hochgeladenen Datei
+const upload = multer({
+  storage: multer.diskStorage({ 
+    destination: './upload/', 
+    filename: function (req, file, cb) {
+      cb(null, file.originalname); 
     },
   }),
 });
@@ -22,9 +22,9 @@ const upload = multer({ // Konfigurieren von Multer
 
 
 
-// Routen-Endpunkt für das Hochladen der Datei definieren
+
 app.post('/uploads', upload.single('excel-file'), (req, res) => {
-    // Die hochgeladene Datei ist jetzt unter req.file verfügbar
+
     const filePath = req.file.path;
     console.log(req.file);
     const filePathHtml = path.join(__dirname, './returns/loading.html');
@@ -32,7 +32,7 @@ app.post('/uploads', upload.single('excel-file'), (req, res) => {
     execute(filePath);
   });
 
-// Server starten
+
 app.listen(3000, () => {
   console.log('Server gestartet auf Port 3000');
 });
